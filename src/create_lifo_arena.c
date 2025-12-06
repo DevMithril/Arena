eb367d5e6f4ab64436d6cdca303d772804d02c69
DevMithril <tbruyelles@gmail.com>
2025-12-06 12:11:21 +0100

    Petite amélioration de performance pour free_lifo(). Nettoyage du code des fonction de l'arena LIFO.

--- src/create_lifo_arena.c
+++ src/create_lifo_arena.c
@@ -3,9 +3,11 @@
 Arena_LIFO *create_lifo_arena(size_t capacity, void *allocator(size_t))
 {
     Arena_LIFO *arena = allocator(capacity + sizeof(Arena_LIFO));
-    if (!arena) return NULL;
-
-    arena->_end_of_arena = (size_t)arena->_data + capacity;
-    arena->_alloc_ptr = (size_t)arena->_data;
+    
+    if (arena)
+    {
+        arena->_end_of_arena = (size_t)arena->_data + capacity;
+        arena->_alloc_ptr = (size_t)arena->_data;
+    }
     return arena;
 }
--- src/malloc_lifo.c
+++ src/malloc_lifo.c
@@ -4,11 +4,10 @@ void *malloc_lifo(Arena_LIFO *arena, size_t size)
 {
     size_t ptr = arena->_alloc_ptr;
     
-    if (ptr + size > arena->_end_of_arena)
+    if (ptr + size <= arena->_end_of_arena)
     {
-        return NULL;
+        arena->_alloc_ptr += size;
+        return (void*)ptr;
     }
-    arena->_alloc_ptr += size;
-
-    return (void*)ptr;
+    return NULL;
 }
